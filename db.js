9f3ae1a2f22e43f42426399dedd0974960f2f0b0
matteodev8 <me@matteodev.xyz>
2024-06-08 13:38:36 +0200

    Added database checks & bump ver

--- db.js
+++ db.js
@@ -1,5 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
 const dbPath = require('./config.json').dbpath;
+
+if (!fs.existsSync(dbPath)) {
+    console.error(`❌ Database error: File '${dbPath}' couldn't be found`);
+    process.exit(1);
+}
 
 let db;
 
@@ -8,13 +14,31 @@ function connectDatabase() {
         db = new sqlite3.Database(dbPath, (err) => {
             console.log("📁 Trying to connect to database...");
             if (err) {
-                console.error("❌ Database error:", err.message);
+                console.error("❌ Database error: ", err.message);
             } else {
+                checkIfTableExists();
                 console.log("✅ Database connected");
             }
         });
     }
     return db;
+}
+
+function checkIfTableExists() {
+    const query = "SELECT name FROM sqlite_master WHERE type='table' AND name='packages';";
+    
+    db.get(query, (err, row) => {
+        if (err) {
+            console.error("❌ Error checking table existence: ", err.message);
+            process.exit(1);
+        }
+        if (row) {
+            console.log("✅ Table 'packages' exists");
+        } else {
+            console.log("❌ Table 'packages' does not exist");
+            process.exit(1);
+        }
+    });
 }
 
 module.exports = connectDatabase();
